9d7ded169214efda63d3013f9e840de7e12184fa
JerryNYang <46272314+JerryNYang@users.noreply.github.com>
2022-10-30 15:26:53 -0500

    Add file to upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Girafee
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number 1: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter a number 2: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("\n\n");
+            Console.WriteLine(num1 + num2);
+
+            //  Console.ReadLine(); if needed pause the console(screen)
+
+
+
+            // mad lib
+            string color;
+            string pluralNoun;
+            string celebrity;
+
+            Console.Write("Enter a color: ");
+            color = Console.ReadLine();
+
+
+            Console.Write("Enter a plural noun: ");
+            pluralNoun = Console.ReadLine();
+
+            Console.Write("Enter a celebrity: ");
+            celebrity = Console.ReadLine();
+
+
+
+            Console.WriteLine("Roses are " + color);
+
+            Console.WriteLine( pluralNoun  + " are blue");
+            Console.WriteLine("I love " + celebrity);
+
+
+        }
+    }
+}
